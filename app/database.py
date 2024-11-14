89e0a1e3692e8d90faa02c8651557fc9f631c0f3
vladick5513 <2006krickunow@gmail.com>
2024-11-15 00:20:29 +0300

    fix import

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
-from config import settings
+from app.config import settings
 
 async_engine = create_async_engine(url=settings.DATABASE_URL_asyncpg, echo=True)
 
