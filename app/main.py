8b56225800da54e31acb6b91eb20be6740453522
vladick5513 <2006krickunow@gmail.com>
2024-11-19 17:18:22 +0300

    add users router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.bookings.router import router as booking_router
+from app.users.router import router as users_router
 
 app = FastAPI()
 
+app.include_router(users_router)
 app.include_router(booking_router)
