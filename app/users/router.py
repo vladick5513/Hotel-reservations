f992955e8481160f21d394a6cf6d22f2e77ed032
vladick5513 <2006krickunow@gmail.com>
2024-11-21 01:44:14 +0300

    add login user

--- app/users/router.py
+++ app/users/router.py
@@ -1,7 +1,7 @@
-from app.users.auth import get_password_hash
+from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
-from app.users.schemas import SUserRegister
-from fastapi import APIRouter, HTTPException
+from app.users.schemas import SUserAuth
+from fastapi import APIRouter, HTTPException, status, Response
 
 router = APIRouter(
     prefix="/auth",
@@ -9,9 +9,20 @@ router = APIRouter(
 )
 
 @router.post("/register")
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email = user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password)
+
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(user_data.email, user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({"sub": user.id})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    return access_token
+
+
