745d10acfebab0965c70f170215fce10b4de1381
vladick5513 <2006krickunow@gmail.com>
2024-11-21 01:43:08 +0300

    add create access token and authenticate user

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,9 @@
+import jwt
+from datetime import datetime, timedelta, timezone
+from app.config import settings
+from app.users.dao import UsersDAO
 from passlib.context import CryptContext
+from pydantic import EmailStr
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +12,17 @@ def get_password_hash(password) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data:dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encode_jwt =jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
+    return encode_jwt
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not user or not verify_password(password, user.hashed_password):  # исправлено
+        return None
+    return user
+
