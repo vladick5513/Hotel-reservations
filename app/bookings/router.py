901b558346b9fa946b610c5362576c8c3940ac37
vladick5513 <2006krickunow@gmail.com>
2024-11-27 19:56:59 +0300

    finished bookings router

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,8 @@
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
-from fastapi import APIRouter
+from app.users.dependencies import get_current_user
+from app.users.models import Users
+from fastapi import APIRouter, Depends
 
 router = APIRouter(
     prefix="/bookings",
@@ -8,8 +10,8 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings() -> list[SBooking]:
-    return await BookingDAO.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
+    return await BookingDAO.find_all(user_id=user.id)
 
 
 
