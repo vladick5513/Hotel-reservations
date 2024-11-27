9fcbe09848e822e55b88477ccaf11d38cde6c257
vladick5513 <2006krickunow@gmail.com>
2024-11-27 19:56:08 +0300

    fix def find_all

--- app/dao/base.py
+++ app/dao/base.py
@@ -21,8 +21,7 @@ class BaseDAO:
             return result.scalar_one_or_none()
 
     @classmethod
-    async def find_all(cls, filter_by=None):
-        filter_by = filter_by or {}
+    async def find_all(cls, **filter_by):
         async with async_session_factory() as session:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
