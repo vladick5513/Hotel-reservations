9333d5fb316f6e0de92b0ca73761a5deab64eade
vladick5513 <2006krickunow@gmail.com>
2024-12-14 15:33:17 +0300

    add  post method

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
+from app.exceptions import RoomCannotBeBooked
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 from fastapi import APIRouter, Depends
@@ -19,6 +20,16 @@ async def get_bookings(
         user: Users = Depends(get_current_user)
 ) -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
+
+@router.post("")
+async def add_bookings(
+        room_id: int, date_from: date, date_to: date,
+        user: Users = Depends(get_current_user)
+):
+
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise RoomCannotBeBooked
 
 
 
