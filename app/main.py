c539440a2ff4a267a631e012710cac1632eaee95
vladick5513 <2006krickunow@gmail.com>
2024-12-14 15:22:27 +0300

    add more routers

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.bookings.router import router as booking_router
 from app.users.router import router as users_router
+from app.pages.router import router as router_pages
+from app.hotels.router import router as hotels_router
+from app.images.router import router as images_router
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(users_router)
 app.include_router(booking_router)
+app.include_router(hotels_router)
+app.include_router(images_router)
+
+
+app.include_router(router_pages)
