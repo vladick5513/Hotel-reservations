4bf9f99ae58adbf3543652e2cff2ceb2aaae30f6
vladick5513 <2006krickunow@gmail.com>
2024-12-14 15:24:17 +0300

    add classmethod in bookings dao

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,5 +1,67 @@
 from app.dao.base import BaseDAO
 from app.bookings.models import Bookings
+from app.database import async_session_factory
+from app.hotels.rooms.models import Rooms
+from sqlalchemy import select, and_, or_, func, insert
+
 
 class BookingDAO(BaseDAO):
     model = Bookings
+
+    @classmethod
+    async def add(
+            cls,
+            user_id,
+            room_id,
+            date_from,
+            date_to,
+    ):
+        async with async_session_factory() as session:
+            booked_rooms = select(Bookings).where(
+                and_(
+                    Bookings.room_id == 1,
+                    or_(
+                        and_(
+                            Bookings.date_from >= date_from,
+                            Bookings.date_from >= date_to,
+                        ),
+                        and_(
+                            Bookings.date_from <= date_from,
+                            Bookings.date_to > date_from,
+                        )
+
+                    )
+                )
+            ).cte("booked_rooms")
+
+            get_rooms_left = select(
+                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label("rooms_left")
+                ).select_from(Rooms).join(
+                booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True
+            ).where(Rooms.id == room_id). group_by(
+                Rooms.quantity, booked_rooms.c.room_id
+            )
+
+            rooms_left = await session.execute(get_rooms_left)
+            rooms_left = rooms_left.scalar()
+
+            if rooms_left > 0:
+                get_price = select(Rooms.price).filter_by(id=room_id)
+                price = await session.execute(get_price)
+                price = price.scalar()
+                add_booking = insert(Bookings).values(
+                    room_id=room_id,
+                    user_id=user_id,
+                    date_from=date_from,
+                    date_to=date_to,
+                    price=price,
+                ).returning(Bookings)
+                new_booking = await session.execute(add_booking)
+                await session.commit()
+                return new_booking.scalar()
+            else:
+                return None
+
+
+
+
