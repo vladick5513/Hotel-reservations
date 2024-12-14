c5115e4bdaf2f50bbb21663f7ec140517ce9e832
vladick5513 <2006krickunow@gmail.com>
2024-12-14 15:32:29 +0300

    add function parameters

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
 from app.users.dependencies import get_current_user
@@ -10,7 +12,12 @@ router = APIRouter(
 )
 
 @router.get("")
-async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking]:
+async def get_bookings(
+        room_id:int,
+        date_from:date,
+        date_to:date,
+        user: Users = Depends(get_current_user)
+) -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
 
 
