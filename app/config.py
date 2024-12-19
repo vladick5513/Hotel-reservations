a161b629bc6e6872ffc4b52125a81b67b3abbd5d
vladick5513 <2006krickunow@gmail.com>
2024-12-19 21:25:12 +0300

    add settings for celery and Redis

--- app/config.py
+++ app/config.py
@@ -9,6 +9,14 @@ class Settings(BaseSettings):
     SECRET_KEY:str
     ALGORITHM:str
 
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: str
+    SMTP_PASS: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+
     @property
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
