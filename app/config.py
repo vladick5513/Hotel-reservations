22cdecac672d6a3fcb34454df77bff9d79468456
vladick5513 <2006krickunow@gmail.com>
2024-12-22 16:47:03 +0300

    prepared the test environment

--- app/config.py
+++ app/config.py
@@ -1,6 +1,10 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings,SettingsConfigDict
 
 class Settings(BaseSettings):
+    MODE: Literal["DEV", "TEST", "PROD"]
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -8,6 +12,20 @@ class Settings(BaseSettings):
     DB_NAME: str
     SECRET_KEY:str
     ALGORITHM:str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
+    @property
+    def TEST_DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
     SMTP_HOST: str
     SMTP_PORT: int
@@ -17,9 +35,9 @@ class Settings(BaseSettings):
     REDIS_HOST: str
     REDIS_PORT: int
 
-    @property
-    def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    SECRET_KEY:str
+    ALGORITHM:str
+
 
     model_config = SettingsConfigDict(env_file=".env", )
 
--- app/database.py
+++ app/database.py
@@ -1,8 +1,19 @@
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+
 from app.config import settings
 
-async_engine = create_async_engine(url=settings.DATABASE_URL_asyncpg, echo=True)
+
+if settings.MODE == "TEST":
+    DATABASE_URL_asyncpg = settings.TEST_DATABASE_URL_asyncpg
+    DATABASE_PARAMS = {"poolclass": NullPool}
+else:
+    DATABASE_URL_asyncpg = settings.DATABASE_URL_asyncpg
+    DATABASE_PARAMS = {}
+
+async_engine = create_async_engine(DATABASE_URL_asyncpg, **DATABASE_PARAMS)
+
 
 async_session_factory = async_sessionmaker(async_engine, expire_on_commit=False)
 
