7f2e63e9577c49f4c5a052caeb00f85b3fcdbff9
vladick <2006krickunow@gmail.com>
2024-12-26 16:25:53 +0300

    add parametrize for test_register_user

--- tests/unit_tests/test_users/test_api.py
+++ tests/unit_tests/test_users/test_api.py
@@ -1,10 +1,16 @@
+import pytest
 from httpx import AsyncClient
 
-
-async def test_register_user(ac: AsyncClient):
+@pytest.mark.parametrize("email, password, status_code", [
+    ("kot@pes.com", "kotopes", 200),
+    ("kot@pes.com", "kot0pes", 409),
+    ("abc", "kot0pes", 422),
+    ("pes@pes.com", "pespes", 200)
+])
+async def test_register_user(email, password, status_code, ac: AsyncClient):
     response = await ac.post("/auth/register", json={
-        "email": "kot@pes.com",
-        "password": "kotopes",
+        "email": email,
+        "password": password,
     })
 
-    assert response.status_code == 200
+    assert response.status_code == status_code
