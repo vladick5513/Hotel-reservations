2c6074b228ac1647410548b8991da9a8f7a431a3
vladick <2006krickunow@gmail.com>
2024-12-26 16:29:28 +0300

    add test_login_user

--- tests/unit_tests/test_users/test_api.py
+++ tests/unit_tests/test_users/test_api.py
@@ -14,3 +14,14 @@ async def test_register_user(email, password, status_code, ac: AsyncClient):
     })
 
     assert response.status_code == status_code
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("test@test.com", "test", 200),
+])
+async def test_login_user(email, password, status_code, ac: AsyncClient):
+    response = await ac.post("/auth/login", json={
+        "email": email,
+        "password": password,
+    })
+
+    assert response.status_code == status_code
